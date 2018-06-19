80ce9894e8a6386c8bb569659f4b292d643c966a
Fatou12 <fatoumatadiallo12222@gmail.com>
2018-06-19 19:44:46 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizz_buzz(int)
+    if int%15==0
+      puts "fizzbuzz"
+    elsif int%5 == 0
+        puts "buzz"
+    elsif int%3 == 0
+      puts "fizz"
+    else
+      puts "nil"
+    end
+end
+
+fizz_buzz(3)
+fizz_buzz(5)
+fizz_buzz(15)
+fizz_buzz(22)
